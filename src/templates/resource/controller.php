8986a2494d2fb510733e23383bb2c6dc1a196056
Randy D. Binondo <binondord@gmail.com>
2015-11-23 09:53:19 +0000

    update

--- src/templates/resource/controller.php
+++ src/templates/resource/controller.php
@@ -13,41 +13,80 @@ class [controller] extends Controller
 		$this->[model] = $[model];
 	}
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 	public function index()
 	{
     	$[models] = $this->[model]->all();
         return view('[model].index', compact('[models]'));
 	}
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 	public function create()
 	{
         return view('[model].create');
 	}
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
 	public function store()
 	{
         $this->[model]->store(\Input::only([repeat]'[property]',[/repeat]));
         return redirect()->route('[model].index');
 	}
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 	public function show($id)
 	{
         $[model] = $this->[model]->find($id);
         return \View::make('[model].show')->with('[model]', $[model]);
 	}
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 	public function edit($id)
 	{
         $[model] = $this->[model]->find($id);
         return view('[model].edit')->with('[model]', $[model]);
 	}
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 	public function update($id)
 	{
         $this->[model]->find($id)->update(\Input::only([repeat]'[property]',[/repeat]));
         return redirect()->route('[model].show', $id)->with('message', 'Item updated successfully.');
 	}
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 	public function destroy($id)
 	{
         $this->[model]->destroy($id);
